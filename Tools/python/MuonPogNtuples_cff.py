74c2780f8d239465bae862c0dec9304ee89e881a
Hugues <hbrun@cern.ch>
2017-02-08 15:48:51 +0100

    few adds to run on miniAOD

--- Tools/python/MuonPogNtuples_cff.py
+++ Tools/python/MuonPogNtuples_cff.py
@@ -29,7 +29,7 @@ def appendMuonPogNtuple(process, runOnMC, processTag="HLT", ntupleFileName="Muon
         process.AOutput.replace(process.reconstruction_step, process.reconstruction_step + process.goodOfflinePrimaryVertices)
     else :
         print "[MuonPogNtuples]: Creating FastFilter path to host goodOfflinePrimaryVertices"
-        process.FastFilters = cms.Path(process.goodOfflinePrimaryVertices)
+        #process.FastFilters = cms.Path(process.goodOfflinePrimaryVertices)
     
     if hasattr(process,"AOutput") :
         print "[MuonPogNtuples]: EndPath AOutput found, appending ntuples"
